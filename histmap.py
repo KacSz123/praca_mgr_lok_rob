71112b0859adcedfc90cd15aae4520ba4a05bdb3
Kacper Szczepanowski <kacperszcz159@gmail.com>
2023-03-09 14:58:36 +0100

    lokalizacja histogramem

---
+++ histmap.py
@@ -0,0 +1,203 @@
+#from rospy_message_converter import json_message_converter
+import rospy
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from mymath import *
+import signal
+import time
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+###################################
+def handler(signum, frame):
+    res = input("\n\nCtrl-c was pressed. Do you really want to exit? y/n ")
+    if res == 'y':
+        exit(1)
+
+signal.signal(signal.SIGINT, handler)
+
+class HistMap():
+    
+    __map = []
+    __orientMap = []
+   # __orientList = []
+    __degrees = []
+    __rawdata = []
+    __scan = []
+    __tmpHist = []
+    __tmpOrient = []
+    __scan_taken = True
+    
+    def __init__(self, nodeName="myNodeHistMap"):
+        #rospy.Subscriber(topicName, LaserScan, self.callback)
+        # self.jsonData = open(fileName)
+        self.__degrees = [math.radians(i/2) for i in range(0,720)]
+        
+        rospy.init_node(nodeName)
+    def loadMap(self,fileName):
+        _scan = []
+        _orient = []
+        _scango = []
+        _pose = [0, 0, 0]
+        json_data = open(fileName)
+        data = json.load(json_data)
+        # print(len(data))
+        # input()
+        _scan = []
+        for j in range(0, len(data)):
+            # Wczytanie skanu z pierwszego zestawu danych
+
+            _scan = []
+            
+            _scango = data[j]["scan"]
+            _pose = data[j]["pose"]
+            for i in range(0, len(_scango)):
+                if (
+                    not math.isnan(_scango[i])
+                    and not math.isinf(_scango[i])
+                ):
+                    _scan.append(_scango[i])
+            self.__rawdata.append((_pose, _scan))
+            self.__orientMap.append(self.generateOrientList(_scango))
+        self.MakeHistMap()
+
+
+    
+    def MakeHistMap(self, binsNumber=20):
+        for i in self.__rawdata:
+            #print(len(i[1]))
+            hist, binno = np.histogram(i[1], range=(0.0, 10.0), bins=binsNumber)
+            self.__map.append((i[0], hist, binno, binsNumber, (0.0, 10.0)))
+            
+    def MakeOrientMap(self, b=20):
+        #print('len orient list ', len(self.__orientList))
+        for scan in self.__rawdata:
+            #print(self.__rawdata)
+            tmpOrientList=[]  
+            for j in range(0,b):
+                sum = 0
+                tmpOrientList=[]    
+                for i in range(0, len(scan[1])//b):
+                        if (
+                            not math.isnan(scan[1][i + b*j])
+                            and not math.isinf(scan[1][i + b*j])
+                            
+                        ):
+                            sum+=scan[1][i+ b*j]
+                tmpOrientList.append(sum/(len(scan[1])//b))
+            
+            self.__orientMap.append(tmpOrientList)
+        #print(self.__orientMap) 
+            
+    def generateOrientList(self,scan,b=20):
+        orientList = []
+        for j in range(0,b):
+            sum = 0
+            counter = 0    
+            for i in range(0, len(scan)//b):
+                    if (
+                        not math.isnan(scan[len(scan)//b + b*j])
+                        and not math.isinf(scan[len(scan)//b + b*j])
+                    ):
+                        counter+=1
+                        sum+=scan[len(scan)//b + b*j]
+            print(counter)
+            orientList.append(sum/(counter))
+        
+        
+        #hist,b2 =np.histogram(self.__tmpOrient, range=(0.0, 6.2831), bins=20)
+        print(len(orientList))
+        return orientList
+    
+    def locateRobot(self):
+        # print(self.__scan)
+        print(len(self.__orientMap ))
+        tmpscan = self.__scan
+        hist1, b = np.histogram(tmpscan, range=(0.0, 10.0), bins=20)
+        currentOrientList= self.generateOrientList(tmpscan)
+        # print(self.__tmpOrient)
+        diffList = []
+        for i in self.__map:
+            diff = 0
+            for j in range(0, 20):
+                diff += abs(i[1][j]-hist1[j])
+            diffList.append(diff)
+            print('', diff)
+        diffList = np.array(diffList)
+        print('Przyblizone polozenie to:', self.__map[np.argmin(diffList)][0])
+        
+        
+        
+        copyCurrentList = currentOrientList.copy()
+        tmpOrient = self.__orientMap[np.argmin(diffList)]
+        diffOrient=[]
+        sum = 0
+        for i in range(0,len(tmpOrient)):
+                sum -= abs(tmpOrient[i]-copyCurrentList[i])
+
+        diffOrient.append(sum)
+        copyCurrentList = copyCurrentList[-1:]+copyCurrentList[:-1]
+        
+        # copyCurrentList = copyCurrentList[-1:]+copyCurrentList[:-1]
+        # print("!!!!!!!!!! ########### !!!!!!!!!!!!!!")
+        # print(len(currentOrientList))
+        # print("!!!!!!!!!! ########### !!!!!!!!!!!!!!")
+        # print(len(tmpOrient))
+        # print("!!!!!!!!!! ########### !!!!!!!!!!!!!!")
+        while currentOrientList != copyCurrentList:
+            sum = 0
+            for i in range(0,len(tmpOrient)):
+                sum -= abs(tmpOrient[i]-copyCurrentList[i])
+
+            diffOrient.append(sum)
+            copyCurrentList = copyCurrentList[-1:]+copyCurrentList[:-1]
+        print(len(diffOrient))
+        orient = 0
+        if math.radians(abs(np.argmin(diffOrient))*18) <=math.pi:
+            orient = math.radians(abs(np.argmin(diffOrient))*18)+math.pi
+        else:
+            orient = math.radians(abs(np.argmin(diffOrient))*18)-math.pi
+        print("Orientacja!!!!!: ", orient)
+
+    def callback(self,msg):
+        self.__scan = msg.ranges
+# 3
+    def printData(self):
+        for i in self.__rawdata:
+            print(i)
+    def printHistMap(self):
+        for i in self.__map:
+            print(i)
+            print('\n')
+    def printHistOrientMap(self):
+        for i in self.__orientMap:
+            print(i)
+            print('\n')
+      
+    
+    def initTopicConnection(self, topicName= '/m2wr/laser/scan' , nodeName ="MyNodeHistMap" ):
+        rospy.Subscriber(topicName, LaserScan, self.callback)
+        
+            
+    def exitTopicConnection(self):
+        self.sub.unregister()
+            
+            
+
+
+def __main__():
+    fileName = 'map11.json'
+    hm = HistMap()
+    hm.initTopicConnection()
+    time.sleep(0.1)
+    hm.loadMap(fileName)
+    #hm.MakeHistMap()
+    # hm.MakeOrientMap()
+    # hm.printHistMap()
+    hm.locateRobot()
+    
+    print('###############################')
+    # hm.printHistOrientMap()
+
+if __name__ == "__main__":
+    __main__()
---
+++ mymath.py
@@ -0,0 +1,98 @@
+import numpy as np
+import math
+from scipy.stats import multivariate_normal
+
+
+def get_yaw_z_degrees(x,y,z,w):
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    return math.degrees(math.atan2(t3, t4))
+def get_yaw_z_radians(x,y,z,w):
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    return math.atan2(t3, t4)
+def GetMuSigma(x):
+    mu_x = np.mean(x)
+    sigma_x = np.std(x)
+    return mu_x, sigma_x*sigma_x
+def gaus2d(x=0, y=0, mx=0, my=0, sx=1, sy=1):
+    return 1. / (2. * np.pi * sx * sy) * np.exp(-((x - mx)**2. / (2. * sx**2.) + (y - my)**2. / (2. * sy**2.)))
+def GetMuSigmaFromEqSqrt(x):
+    suma=0
+    for i in x:
+        suma = suma + i
+    mu_x=sum(x)/len(x)
+    suma=0
+    for i in x:
+        suma = suma + (i-mu_x)*(i-mu_x)
+    sigma_x=suma/len(x)
+    return mu_x, math.sqrt(sigma_x)
+        
+def GetMuSigmaFromEq(x):
+    suma=0
+    for i in x:
+        suma = suma + i
+    mu_x=sum(x)/len(x)
+    suma=0
+    for i in x:
+        suma = suma + (i-mu_x)*(i-mu_x)
+    sigma_x=suma/len(x)
+    return mu_x, sigma_x
+
+
+    
+    
+def RegLinp(x,y,xmi,ymi):
+    sumU=0
+    sumD=0
+    for i in range(len(x)):
+        sumU+=(x[i]*y[i])
+        sumD+=(x[i])**2
+    sumD=(sumD-len(x)*(xmi**2))
+    if sumD==0:
+        sumD=0.001
+    xr=(sumU-(len(x)*xmi*ymi))
+    yr=sumD
+    ang = 0
+    a=xr/yr
+    if xr<0 and yr>0:
+        ang=math.degrees(math.atan2(xr,yr))
+        if a<-1:
+            ang=math.degrees(math.atan2(xr,yr)-math.pi/2)
+    elif xr>0 and yr>0:            
+        ang=math.degrees(math.atan2(xr,yr))
+        if a>1:
+            ang=math.degrees(math.atan2(xr,yr)+math.pi/2)
+    else:            
+        ang=math.degrees(math.atan2(xr,yr))              
+    return ang
+
+def SortBy2(el):
+    return el[1]
+
+def GetBeginEnd(x,y):
+    ll=[]
+    for i in range(len(x)):
+        ll.append((x[i],y[i]))
+    ll.sort(key=SortBy2)
+    xr=x[0]-x[len(x)-1]
+    yr=y[0]-y[len(y)-1]
+    if xr==0:
+        xr=0.01
+    a=yr/xr
+
+    if xr<0 and yr>0:
+        ang=-math.degrees(math.atan2(xr,yr)+math.pi/2)
+        if a<-1:
+            ang=-math.degrees(math.atan2(xr,yr))
+    elif xr>0 and yr>0:            
+        ang=-math.degrees(math.atan2(xr,yr)-math.pi/2)
+        if a>1:
+            ang=-math.degrees(math.atan2(xr,yr))
+                        #print('4hehe')
+    else:            
+        ang=-math.degrees(math.atan2(xr,yr))
+    return ang
+
+def Dist2Points(p1,p2):
+    return math.sqrt(((p1[0]-p2[0])**2)+((p2[1]-p2[1])**2))
